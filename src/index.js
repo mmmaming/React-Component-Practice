45c7a7f4a35fc695cafa58631d81e47476ef68a1
mmmaming <864845434@qq.com>
2017-11-29 16:57:08 +0800

    添加性能测试

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './example';
-import registerServiceWorker from './registerServiceWorker';
 
+import registerServiceWorker from './registerServiceWorker';
+import Perf from 'react-addons-perf'
+window.Perf = Perf;
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
